52d65d858c49e05c4ad7517109ee0b2cdb580740
timbeck2 <timbeck2@gmail.com>
2015-05-25 13:44:14 -0500

    Added in Deposite and withdrawal code

--- Android/TecsupMobileBankingProject/app/src/main/java/edu/purdue/tech/tecsupmobilebankingproject/Deposit.java
+++ Android/TecsupMobileBankingProject/app/src/main/java/edu/purdue/tech/tecsupmobilebankingproject/Deposit.java
@@ -4,6 +4,15 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Deposit extends ActionBarActivity {
@@ -19,6 +28,52 @@ public class Deposit extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_deposit, menu);
+
+
+
+        final Button btn;
+        final EditText eText;
+        btn = (Button) findViewById(R.id.btnDepositSubmit);
+        eText = (EditText) findViewById(R.id.deposit_amount);
+
+
+        List<String> spinnerArray =  new ArrayList<String>();
+        spinnerArray.add("Checking");
+        spinnerArray.add("Savings");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        final Spinner sItems = (Spinner) findViewById(R.id.spin_account);
+        sItems.setAdapter(adapter);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+                //msg.show();
+
+                String selected = sItems.getSelectedItem().toString();
+                if (selected.equals("Checking")) {
+                    String account_str = "Checking";
+                    Toast account_msg = Toast.makeText(getBaseContext(),account_str,Toast.LENGTH_LONG);
+                    account_msg.show();
+                }
+                if (selected.equals("Savings")) {
+                    String account_str = "Savings";
+                    Toast account_msg = Toast.makeText(getBaseContext(),account_str,Toast.LENGTH_LONG);
+                    account_msg.show();
+                }
+
+            }
+        });
+
+
+
+
         return true;
     }
 
