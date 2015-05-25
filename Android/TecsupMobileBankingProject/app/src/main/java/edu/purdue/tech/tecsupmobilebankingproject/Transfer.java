e45114b0b3b0637c0909eca7fcde9eec9a5e4604
vince94 <vincekye@gmail.com>
2015-05-25 13:15:14 -0500

    Transfer activity UI completion
    Spinners now work properly, "Request Transfer" button logic planned out

--- Android/TecsupMobileBankingProject/app/src/main/java/edu/purdue/tech/tecsupmobilebankingproject/Transfer.java
+++ Android/TecsupMobileBankingProject/app/src/main/java/edu/purdue/tech/tecsupmobilebankingproject/Transfer.java
@@ -1,5 +1,4 @@
 package edu.purdue.tech.tecsupmobilebankingproject;
-
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,13 +6,33 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.math.BigDecimal;
 
 public class Transfer extends ActionBarActivity
 {
+    public void transferClickEvent()
+    {
+        //Get text from the Amount TextView and save it into moneyRequest
+        TextView amountTextBox = (EditText)findViewById(R.id.txtAmount);
+        if (amountTextBox.getText().toString()=="")
+        {
+            //Make toast for "Please enter an amount."
+        }
+        else
+        {
+            BigDecimal moneyRequest = new BigDecimal(amountTextBox.getText().toString());
+
+            //Subtract this with the amount of money actually in the database
+            //If the result of this is negative, set "@+id/txtAmount" to "", show "There's not that much money in your account." in a Toast pop-up, and stop execution of code
+            //Apply the transfer to the database via SQL?
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -36,22 +55,23 @@ public class Transfer extends ActionBarActivity
                     @Override
                     public void onItemSelected(
                             AdapterView<?> parent, View view, int position, long id) {
-                        Toast.makeText(getBaseContext(), "Spinner1: position=" + position + " id=" + id, Toast.LENGTH_SHORT).show();
 
-                    ////////////////////////////////////////////////////////////////////////
-                    //This is happening in the onCreate, not in a click event like it should
-                        if (combo.findViewById(R.id.spinnerFrom).equals(0))
+                    //Automagically set the spinner's position.
+                        if (position==0)
+                        {
                             combo2.setSelection(1);
-                        if (combo.findViewById(R.id.spinnerFrom).equals(1))
+                            //Get the current amount of money in Checking from the database and display it in "@+id/CurrentBalanceLabel"
+                        }
+                        if (position==1)
+                        {
                             combo2.setSelection(0);
-
+                            //Get the current amount of money in Savings from the database and display it in "@+id/CurrentBalanceLabel"
+                        }
                     }
                     @Override
                     public void onNothingSelected(AdapterView<?> parent) {
-
                     }
                 });
-
     }
 
     @Override
@@ -72,7 +92,6 @@ public class Transfer extends ActionBarActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) return true;
-
         return super.onOptionsItemSelected(item);
     }
 }
